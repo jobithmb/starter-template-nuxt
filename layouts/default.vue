6c3e8a663960079ea6a415e8d6fb9d39a8de2ca4
Jobith M Basheer <jobith@three38inc.com>
2023-02-08 13:29:48 +0530

    added layout files

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout'
+}
+</script>
+
+<style lang="scss">
+.app-layout {
+    background-color: $background-color;
+}
+</style>
